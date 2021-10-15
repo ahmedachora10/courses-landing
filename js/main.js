da22098c2c441f1f5cdc199d063f68f23079c268
ahmedachora10 <gjr8195@gmail.com>
2021-10-15 17:58:58 +0200

    init commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+const courses = {
+    coursesContainer: document.querySelector('#courses-container'),
+    coursesCount: document.querySelector('#courses-container').childElementCount,
+    courseWidth: 200,
+    counter: 0,
+    nextBtn: document.querySelector('#next'),
+    prevBtn: document.querySelector('#prev'),
+
+    run() {
+        this.coursesContainer.setAttribute('style', `width: ${this.coursesCount * this.courseWidth}`);
+
+        this.nextBtn.addEventListener('click', () => this.next());
+
+        this.prevBtn.addEventListener('click', () => this.counter > 0 ? this.prev() : false);
+    },
+
+    move() {
+        this.coursesContainer.setAttribute('style', `transform: translateX(-${this.courseWidth * this.counter}px)`);
+    },
+
+    next() {
+        this.counter++;
+        this.move();
+    },
+
+    prev() {
+        this.counter--;
+        this.move();
+    }
+}
+
+courses.run();
